4f3dae1ba444bddadfb13c42734771b77d7b10dc
mihmilicio <milenamilicio@hotmail.com>
2022-05-26 15:59:35 -0300

    Adding images to the list items

--- app/src/main/java/io/github/mihmilicio/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/io/github/mihmilicio/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import io.github.mihmilicio.affirmations.R
@@ -26,11 +27,13 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount() = dataset.size
 
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 }
