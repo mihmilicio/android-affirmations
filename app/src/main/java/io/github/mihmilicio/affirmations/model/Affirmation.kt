ebb29491cc0b62af4b5b047f0c59b45822154f6f
mihmilicio <milenamilicio@hotmail.com>
2022-05-26 16:15:02 -0300

    Change app theme colors and icon

--- app/src/main/java/io/github/mihmilicio/affirmations/model/Affirmation.kt
+++ app/src/main/java/io/github/mihmilicio/affirmations/model/Affirmation.kt
@@ -5,5 +5,6 @@ import androidx.annotation.StringRes
 
 data class Affirmation(
     @StringRes val stringResourceId: Int,
-    @DrawableRes val imageResourceId: Int) {
+    @DrawableRes val imageResourceId: Int
+) {
 }
